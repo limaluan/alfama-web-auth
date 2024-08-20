16382e6dac0a2d967ea63c318653718fa8f38671
Luan Lima <limaluan.dev@gmail.com>
2024-08-20 20:40:25 -0300

    First Commit

---
+++ initdb/create-tables.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  full_name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  phone VARCHAR(20),
+  cpf VARCHAR(14) UNIQUE,
+  company VARCHAR(255),
+  address TEXT
+);
